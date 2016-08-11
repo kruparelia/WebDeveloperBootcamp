9a63aa7473d915646ece215662e8ea576e59e67d
Kunal Ruparelia <Kruparelia8416@conestogac.on.ca>
2016-08-10 21:52:08 -0400

    ,

---
+++ 8-IntroToJavaScript/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("First name?");
+var lastName = prompt("Last name?");
+var age = prompt("Age?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old.")
